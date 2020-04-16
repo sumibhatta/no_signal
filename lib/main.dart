eeba2240340894197508cdea96819da01c16bee9
sumibhatta <sumibhatta@kbc.edu.np>
2020-04-16 08:53:23 +0545

    second

---
+++ lib/main.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          backgroundColor: Colors.white,
+          body: SafeArea(
+            child: Column(
+              children: <Widget>[
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      width: 56.10385714,
+                      height: 500.0,
+                      color: Colors.white,
+                    ),
+                    Container(
+                      width: 56.10385714,
+                      height: 500.0,
+                      color: Color.fromRGBO(255, 255, 1, 1),
+                    ),
+                    Container(
+                      width: 56.10385714,
+                      height: 500.0,
+                      color: Color.fromRGBO(84, 221, 255, 1),
+                    ),
+                    Container(
+                      width: 56.10385714,
+                      height: 500.0,
+                      color: Color.fromRGBO(0, 255, 1, 1),
+                    ),
+                    Container(
+                      width: 56.10385714,
+                      height: 500.0,
+                      color: Color.fromRGBO(255, 0, 205, 1),
+                    ),
+                    Container(
+                      width: 56.10385714,
+                      height: 500.0,
+                      color: Color.fromRGBO(213, 1, 0, 1),
+                    ),
+                    Container(
+                      width: 56.10385714,
+                      height: 500.0,
+                      color: Colors.blue,
+                    ),
+                  ],
+                ),
+                Row(
+                  
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      width: 56.10385714,
+                      height: 100.0,
+                      color: Colors.blue,
+                    ),
+                    Container(
+                      width: 56.10385714,
+                      height: 100.0,
+                      color: Colors.black,
+                    ),
+                    Container(
+                      width: 56.10385714,
+                      height: 100.0,
+                      color: Color.fromRGBO(255, 0, 205, 1),
+                    ),
+                    Container(
+                      width: 56.10385714,
+                      height: 100.0,
+                      color: Colors.black,
+                    ),
+                    Container(
+                      width: 56.10385714,
+                      height: 100.0,
+                      color: Colors.white,
+                    ),
+                    Container(
+                      width: 56.10385714,
+                      height: 100.0,
+                      color: Colors.black,
+                    ),
+                    Container(
+                      width: 56.10385714,
+                      height: 100.0,
+                      color: Colors.grey,
+                    ),
+                  ],
+
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      width: 76.10385714,
+                      height: 113.0,
+                      color: Color.fromRGBO(84, 221, 255, 1),
+                    ),
+                    Container(
+                      width: 76.10385714,
+                      height: 113.0,
+                      color: Colors.white,
+                    ),
+                    Container(
+                      width: 76.10385714,
+                      height: 113.0,
+                      color: Colors.blue,
+                    ),
+                    Container(
+                      width: 164.4154286,
+                      height: 113.0,
+                      color: Colors.black,
+                    ),
+                  ],
+
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
